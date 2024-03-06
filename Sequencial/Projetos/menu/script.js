859f281a263a93f7ef341c2e5d9833140ce4c5a1
Lavínia Sá <82878705+laviniasa@users.noreply.github.com>
2024-03-06 09:13:32 -0300

    Add files via upload

---
+++ Sequencial/Projetos/menu/script.js
@@ -0,0 +1,9 @@
+document.getElementById("btn").addEventListener("click", function() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "none") {
+      menu.style.display = "block";
+    } else {
+      menu.style.display = "none";
+    }
+  });
+  
