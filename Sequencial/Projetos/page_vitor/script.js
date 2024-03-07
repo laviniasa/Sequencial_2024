7ccb3303b3f73ba09407e19aa178fe0905855a9a
Lavínia Sá <82878705+laviniasa@users.noreply.github.com>
2024-03-07 08:29:32 -0300

    Add files via upload

---
+++ Sequencial/Projetos/page_vitor/script.js
@@ -0,0 +1,9 @@
+document.getElementById("btn").addEventListener("click",function(){
+    window.location.href = "index.html";
+});
+document.getElementById("btn2").addEventListener("click",function(){
+    window.location.href = "serviços.html";
+});
+document.getElementById("btn3").addEventListener("click",function(){
+    window.location.href = "contato.html";
+});
